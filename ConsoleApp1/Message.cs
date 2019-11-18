335c98c35225c9b1b90d6a04aa7a9706a9dbb855
vivienkoelsch88 <vivien.koelsch@le-campus-numerique.fr>
2019-11-18 14:04:31 +0100

    first commit

---
+++ ConsoleApp1/Message.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Message
+    {
+
+        private String message = "";
+        private int matin;
+        private int midi;
+        private int soir;
+
+        public Message(int matin =  9, int midi = 13, int soir = 18)
+        {
+            this.matin = matin;
+            this.midi = midi;
+            this.soir = soir;
+        }
+
+        public String GetHelloMessage()
+        {
+            DateTime dateValue = DateTime.Now;
+            
+            if (dateValue.DayOfWeek == DayOfWeek.Saturday || dateValue.DayOfWeek == DayOfWeek.Sunday || (dateValue.DayOfWeek == DayOfWeek.Friday && (dateValue.Hour >= this.soir || dateValue.Hour < this.matin)))
+            {
+                this.message = "Bon week-end ";
+            }
+            else if (dateValue.Hour >= this.matin && dateValue.Hour <= this.midi)
+            {
+                this.message = "Bonjour ";
+            }
+            else if (dateValue.Hour > this.midi && dateValue.Hour < this.soir)
+            {
+                this.message = "Bon après midi ";
+            }
+            else if (dateValue.Hour >= 18 || dateValue.Hour < this.matin)
+            {
+                this.message = "Bonsoir ";
+            }
+            
+
+            message = this.message + System.Environment.UserName + " !";
+
+            return this.message;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new Message();
+
+            Console.WriteLine(message.GetHelloMessage());
+
+            while (true) { 
+            //ConsoleKeyInfo saisie = Console.ReadKey(true);
+                String text  = Console.ReadLine();
+                //if (saisie.Key == ConsoleKey.Enter)
+                if (text == "")
+                {
+                    
+                    Console.WriteLine(message.GetHelloMessage());
+                    
+                }
+                else if (text == "exit")
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
